6402abc05415669a7177a14d1af97105c69ef72b
ShivSubedi <>
2020-12-27 16:39:27 -0500

    hello world

---
+++ w1_p4.py
@@ -0,0 +1,12 @@
+//Shiv Subedi
+//This snippet of code shows printing simple plain texts 
+
+def do_stuff():
+    """
+    Example of print vs. return
+    """
+    print("Hello world")
+    return "Is it over yet?"
+    print("Goodbye cruel world!")
+
+print(do_stuff())
