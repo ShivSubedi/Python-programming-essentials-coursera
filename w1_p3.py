3f8704eeb275ab877b8d2529d61599b9ce4a0122
ShivSubedi <>
2020-12-27 16:35:47 -0500

    scale x- and y- to actual values

---
+++ w1_p3.py
@@ -0,0 +1,9 @@
+//Shiv Subedi
+//This snippet of code shows how to scale to the actual x- and y- values
+
+def project_to_distance(point_x, point_y, distance):
+    dist_to_origin = (point_x ** 2 + point_y ** 2) ** 0.5
+    scale = distance / dist_to_origin
+    print (point_x * scale, point_y * scale)
+
+project_to_distance(2, 7, 4)
