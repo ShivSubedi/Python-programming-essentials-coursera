7b7f105fdd196da03b51fae90e468263d87e8665
ShivSubedi <>
2020-12-27 17:09:38 -0500

    calculate compound interest and amount

---
+++ w1_p6.py
@@ -0,0 +1,18 @@
+#Shiv Subedi
+#This snippet of code shows how to calculate compound interest and amount
+
+def future_value(present_value, annual_rate, periods_per_year, years):
+    """
+    Input: the numbers present_value, annual_rate, periods_per_year, years
+    Output: future value based on formula given in question
+    """
+    rate_per_period = annual_rate / periods_per_year
+    periods = periods_per_year * years
+
+
+    # Put your code here.
+    fv=present_value*(1+rate_per_period)**periods
+    return fv
+
+
+print("$1000 at 2% compounded daily for 4 years yields $", future_value(1000, .02, 365, 4))
